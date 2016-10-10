fb47cc27d3a083b407ec6c80367b565734d15a26
jangbohyun <pinkporker@gmail.com>
2016-10-10 16:16:54 +0900

    login logout 완료 세션 정보 가져와서 user Nickname 출력완료

---
+++ SpringEdu/src/main/java/com/spring/edu/model/UserModel.java
@@ -0,0 +1,36 @@
+package com.spring.edu.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserModel {
+
+	String userName;
+	String password;
+	String nickName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
